bee7f16078d617750d9fbfdebf0356dbeee06a8a
Alex <alex@gmail.com>
2021-08-22 10:58:42 +0200

    first commit

--- data.ts
+++ data.ts
@@ -1,7 +1,7 @@
 import { RiComputerLine } from "react-icons/ri";
 import { FaServer } from "react-icons/fa";
-import { AiOutlineAntDesign, AiOutlineApi } from "react-icons/ai";
-import { MdDeveloperMode } from "react-icons/md";
+import { AiOutlineApi } from "react-icons/ai";
+
 import { IProjet, Service, Skill } from "./types";
 
 import { BsCircleFill } from "react-icons/bs";
